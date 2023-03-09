feb0a6136eca9a00cdd8448a2d04ae29df6128bb
Ladányi Levente <126462743+1tc-ladlev@users.noreply.github.com>
2023-03-09 09:49:51 +0100

    2.feladat kész

--- megoldas.py
+++ megoldas.py
@@ -19,7 +19,8 @@ for het in hianyzasok:
 
 print(f"1. feladat: {osszeg} óra hiányzás volt összesen")
 
+
 # 2. Volt-e olyan hét, amikor nem volt hiányzó?
 # 2. feladat: Nem volt olyan hét, amikor nem volt hiányzó
 
-print(f"2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
+print("2. feladat: Nem volt olyan hét, amikor nem volt hiányzó")
